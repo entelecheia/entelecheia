79fd6eb8df7117a4f9e0abf31a0c2c718c2f6012
Young Joon Lee <entelecheia@hotmail.com>
2023-07-27 07:07:26 +0900

    fix(entelecheia): change initialization methods for HyFI and logging

--- src/entelecheia/__init__.py
+++ src/entelecheia/__init__.py
@@ -1,19 +1,26 @@
 import os
 
-import hyfi
+from hyfi import HyFI
 
 from ._version import __version__
 
-# Read the package name from the current directory
-__package_name__ = os.path.basename(os.path.dirname(__file__))
+# Read the package path from the current directory
+__package_path__ = os.path.dirname(__file__)
 
 # Initialize the global HyFI object
-hyfi.initialize_global_hyfi(package_name=__package_name__, version=__version__)
+HyFI.initialize_global_hyfi(
+    package_path=__package_path__,
+    version=__version__,
+    plugins=[],
+)
 
 # Initialize the logger
-hyfi.HyFI.setLogger()
+HyFI.setLogger()
 
 
 def get_version() -> str:
     """Get the package version."""
     return __version__
+
+
+__all__ = ["HyFI", "get_version"]
